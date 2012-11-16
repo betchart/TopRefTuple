660f6359e591829eb7f63367ea7003a5b23a8e04
bbetchar <>
2012-11-16 18:52:31 +0000

    add number of good vertices

--- interface/Tuple_Event.h
+++ interface/Tuple_Event.h
@@ -3,12 +3,14 @@
 
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 
 class Tuple_Event : public edm::EDProducer {
  public: 
   explicit Tuple_Event(const edm::ParameterSet&);
  private: 
   void produce( edm::Event &, const edm::EventSetup & );
+  const edm::InputTag vertexTag;
 };
 
 #endif
--- plugins/Tuple_Event.cc
+++ plugins/Tuple_Event.cc
@@ -1,7 +1,12 @@
 #include "TopQuarkAnalysis/TopRefTuple/interface/Tuple_Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
 #include "FWCore/Framework/interface/Event.h"
 
-Tuple_Event::Tuple_Event(const edm::ParameterSet& iConfig) {
+Tuple_Event::Tuple_Event(const edm::ParameterSet& cfg)
+  : vertexTag(cfg.getParameter<edm::InputTag>("vertexTag"))
+{
   produces <bool>         ( "isRealData"  );
   produces <unsigned int> ( "run"   );
   produces <unsigned int> ( "event" );
@@ -9,29 +14,24 @@ Tuple_Event::Tuple_Event(const edm::ParameterSet& iConfig) {
   produces <unsigned int> ( "bunch" );
   produces <unsigned int> ( "orbit" );
   produces <double>       ( "time" );
+  produces <unsigned>     ( "nVertex" );
 }
 
 void Tuple_Event::
-produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-
-  std::auto_ptr<bool >     isRealData ( new bool(        iEvent.isRealData()      ) );
-  std::auto_ptr<unsigned int >  run   ( new unsigned int(iEvent.id().run()        ) );
-  std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()      ) );
-  std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.luminosityBlock() ) );
-  std::auto_ptr<unsigned int >  bunch ( new unsigned int(iEvent.bunchCrossing()   ) );
-  std::auto_ptr<unsigned int >  orbit ( new unsigned int(iEvent.orbitNumber()     ) );
+produce(edm::Event& evt, const edm::EventSetup& iSetup) {
+  edm::Handle<edm::View<reco::Vertex> > vertices;  evt.getByLabel(vertexTag, vertices);
   
-  double sec  = iEvent.time().value() >> 32 ;
-  double usec = 0xFFFFFFFF & iEvent.time().value();
-  double conv = 1e6;
+  double 
+    sec(evt.time().value() >> 32),
+    usec(0xFFFFFFFF & evt.time().value()),
+    conv(1e6);
 
-  std::auto_ptr<double >        time  	      ( new double(sec+usec/conv));
-
-  iEvent.put( isRealData, "isRealData" );
-  iEvent.put( run,   "run"   );
-  iEvent.put( event, "event" );
-  iEvent.put( ls   , "lumiSection" );
-  iEvent.put( bunch, "bunch" );
-  iEvent.put( orbit, "orbit" );
-  iEvent.put( time,  "time"  );
+  evt.put( std::auto_ptr<bool >   ( new bool( evt.isRealData()         ) ), "isRealData");
+  evt.put( std::auto_ptr<unsigned>( new unsigned(evt.id().run()        ) ), "run" );
+  evt.put( std::auto_ptr<unsigned>( new unsigned(evt.id().event()      ) ), "event");
+  evt.put( std::auto_ptr<unsigned>( new unsigned(evt.luminosityBlock() ) ), "lumiSection");
+  evt.put( std::auto_ptr<unsigned>( new unsigned(evt.bunchCrossing()   ) ), "bunch");
+  evt.put( std::auto_ptr<unsigned>( new unsigned(evt.orbitNumber()     ) ), "orbit");
+  evt.put( std::auto_ptr<unsigned>( new unsigned( vertices.isValid() ? vertices->size() : 0 ) ), "nVertex");
+  evt.put( std::auto_ptr<double > ( new double(sec+usec/conv)), "time");
 }
