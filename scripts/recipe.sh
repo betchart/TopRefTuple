37d5793973980ededa3f22badc9c540bf5469e06
bbetchar <>
2012-11-16 21:12:06 +0000

    addpkg syntax

--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -2,8 +2,8 @@ RELEASE=CMSSW_5_3_6_patch1
 cmsrel $RELEASE
 cd $RELEASE/src
 cmsenv
-cvs co -r V00-00-08    RecoMET/METAnalyzers
-cvs co -r V15-02-06    RecoParticleFlow/PFProducer
+addpkg RecoMET/METAnalyzers  V00-00-08
+addpkg RecoParticleFlow/PFProducer V15-02-06
 cvs co              -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
 cvs co -r V00-00-13 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
 cd EGamma/EGammaAnalysisTools/data
