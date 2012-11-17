d5352e846a7a071c9356301b9869ce8fe70be2ac
bbetchar <>
2012-11-17 00:21:38 +0000

    bookkeeping scripts

---
+++ scripts/bookkeeping/claimJob.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+
+import configuration_SCBooks as conf,sys,os,readline,getpass,string,fileinput,socket,datetime,re
+
+def remove_multislash(inString) :  return re.sub(r'//*','/',inString)
+
+def print_and_execute(c) : print c; os.system(c)
+
+def get_jobrow(db) :
+    rows = db.execute('''select job.rowid,recipe,globalTag,dataset
+                       from job join dset on dset.rowid=job.dsetid
+                       where state="Unclaimed"''').fetchall()
+    for i,row in enumerate(rows):
+        print '\n'.join(['', row.keys(),
+                         '\t'.join([str(item) for item in row])[0:120] + "..."][None if i%10 else 2:])
+    jobnumber = raw_input("\n\n\tWhich job?  ")
+    if not jobnumber in [str(row['rowid']) for row in rows] :
+        print jobnumber+' is not an `Unclaimed` job'
+        db.disconnect()
+        sys.exit()
+    
+    return db.execute('''select job.rowid,jsonls,recipe,dataset,isData,globalTag,nonDefault
+                         from job join dset on dset.rowid=job.dsetid
+                         where state="Unclaimed" AND job.rowid='''+jobnumber).fetchone()
+
+def setup_cmssw(job,path) :
+    print_and_execute('\n'.join(["#!/usr/bin/env bash",
+                                 "mkdir -p %s"%path,
+                                 "cd %s"%path,
+                                 "cvs co -drecipes -r %s UserCode/Betchart/TopRefTuple/scripts/recipe.sh"%job['recipe'],
+                                 "cat recipes/recipe.sh",
+                                 ". recipes/recipe.sh",
+                                 "scram b -j 8",
+                                 'echo "\n\n\nCheck that everything built:"',
+                                 'scram b']))
+
+def setup_output_dirs(option) :
+    dirs = option["FULL_RPATH"].strip('/').split('/')
+    print_and_execute('\n'.join(['#!/usr/bin/env bash',
+                                 'rfmkdir -p %(FULL_RPATH)s'%option,
+                                 'rfchmod 775 %(FULL_RPATH)s'%option]+
+                                ['rfchmod 755 /'+'/'.join(dirs[:-i]) for i in range(1,len(dirs)-dirs.index(option["USER"]))])
+                      )
+
+
+def setup_crab(job,option) :
+
+    SITE = { "CASTOR" : {"SE":"srm-cms.cern.ch",
+                         "FULL_RPATH":"/castor/cern.ch/user/%(INITIAL)s/%(USER)s/%(RPATH)s",
+                         "USER_REMOTE": "user/%(INITIAL)s/%(USER)s/%(RPATH)s",
+                         "SCHEDULER":"glite",
+                         "DBS_URL": option["DBS_URL"],
+                         "EXTRA": "\n[USER]\nstorage_path=/srm/managerv2?SFN=/castor/cern.ch"
+                         },
+             "CAF"    : {"SE":"T2_CH_CAF",
+                         "FULL_RPATH":"/castor/cern.ch/cms/store/caf/user/%(USER)s/%(RPATH)s" % option,
+                         "USER_REMOTE":"%(RPATH)s",
+                         "SCHEDULER":"caf",
+                         "DBS_URL": (option["DBS_URL"] if option["DBS_URL"] \
+                                     else None if job['dataset'].find('ExpressPhysics')<0 \
+                                     else "http://cmsdbsprod.cern.ch/cms_dbs_caf_analysis_01/servlet/DBSServlet"),
+                         "EXTRA":"\n[CAF]\nqueue=cmscaf1nd\n"
+                         },
+             "LONDON" : {"SE":"T2_UK_London_IC",
+                         "FULL_RPATH":"/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/%(USER)s/%(RPATH)s" % option,
+                         "USER_REMOTE":"%(RPATH)s",
+                         "SCHEDULER":"glidein",
+                         "DBS_URL": option["DBS_URL"],
+                         "EXTRA": ""},
+             "OSETHACK" : {"SE":"T2_UK_London_IC",
+                           "FULL_RPATH":"/pnfs/hep.ph.ic.ac.uk/data/cms/store/user/%(USER)s/%(RPATH)s" % option,
+                           "USER_REMOTE":"%(RPATH)s",
+                           "SCHEDULER":"glidein",
+                           "DBS_URL": "http://cmsdbsprod.cern.ch/cms_dbs_ph_analysis_02/servlet/DBSServlet",
+                           "EXTRA": "[CRAB]\nserver_name=slc5ucsd\n[GRID]\nse_white_list=T1,T2\nce_white_list=T1,T2\n"},
+              "FNAL" : {"SE":"cmssrm.fnal.gov",
+              		"FULL_RPATH":"/pnfs/cms/WAX/11/store/user/lpcsusyra1/%(USER)s/%(RPATH)s" % option,
+              		"USER_REMOTE":"/store/user/lpcsusyra1/%(USER)s/%(RPATH)s",
+              		"SCHEDULER":"glidein",
+                        "DBS_URL": option["DBS_URL"],
+                        "EXTRA":""}
+             }
+             
+    option["INITIAL"] = option["USER"][0]
+    for key,val in SITE[option["SITE"]].items() :
+        if val is None : continue
+        option[key] = eval("'''%s'''%option"%val)
+
+    option["EVENTS"] = '\n'.join(['lumis_per_job=150',
+                                  'total_number_of_lumis=-1',
+                                  'lumi_mask=%(PATH)s/jsonls.txt'%option if job['jsonls'] else ''
+                                  ] if job['isData'] else
+                                 ['total_number_of_events=-1',
+                                  'events_per_job=100000'
+                                  ]
+                                 )
+    
+    option["DBS_URL"] = ("dbs_url="+option["DBS_URL"]) if option["DBS_URL"] else ""
+
+    if option["SITE"] not in ["LONDON","OSETHACK","FNAL"]: setup_output_dirs(option)
+    if job['jsonls'] :
+        with open("%(PATH)s/jsonls.txt"%option,"w") as jsonfile:
+            print>>jsonfile,str(job['jsonls']).replace("'",'"')
+
+    option["FNAL_SPECIFIC"] = "" if option['SITE'] != "FNAL" else '\n'.join(['check_user_remote_dir = 0',
+                                                                             'storage_path = /srm/managerv2?SFN=11'])
+    with open("%(PATH)s/crab.cfg"%option,"w") as crabfile :
+        print>>crabfile,'''
+[CMSSW]
+get_edm_output = 1
+%(EVENTS)s
+pset=topTuple_cfg.py
+datasetpath=%(DATASET)s
+%(DBS_URL)s
+
+[GRID]
+virtual_organization=cms
+
+[USER]
+copy_data=1
+user_remote_dir=%(USER_REMOTE)s
+storage_element=%(SE)s
+%(FNAL_SPECIFIC)s
+
+[CRAB]
+cfg=crab.cfg
+scheduler=%(SCHEDULER)s
+use_server=%(SERVER)d
+jobtype=cmssw
+
+%(EXTRA)s
+'''% option
+    return
+
+
+def setup_multi(job,path) :
+    with open(path+"/multicrab.cfg","w") as mcrabfile :
+        print>>mcrabfile, '\n'.join(['[MULTICRAB]',
+                                     'cfg=crab.cfg'] +
+                                    [ '\n'.join(['',
+                                                 '[%s]'% ds.strip('/').replace(" ","_").replace("/","."),
+                                                 'CMSSW.datasetpath=%s'%ds])
+                                      for ds in job['DATASET'].split(',')])
+
+def run_crab(job,path,MULTI,onlyTest) :
+    print_and_execute('''
+#!/usr/bin/env bash
+    
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+cd %(path)s/%(cmssw)s/src/
+eval `scram runtime -sh`
+source %(crab_setup)s
+cd %(path)s
+python %(cmssw)s/src/TopQuarkAnalysis/TopRefTuple/test/config.py isData=%(isData)d globalTag=%(gt)s %(other)s
+%(crab)s -create
+%(doit)s
+%(crab)s -status &> crab.status
+'''%{ "cmssw" : os.environ['CMSSW_BASE'],
+      "isData" : job['isData'],
+      "gt" : job['globalTag'],
+      "other" : job['nonDefault'] if job['nonDefault'] else '',
+      "crab" : "multicrab" if MULTI else "crab",
+      "crab_setup" : "/afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh",
+      "doit" : "" if onlyTest else "%(crab)s -submit"
+      })
+    return
+
+def get_dashboard(path) :
+    dash = []
+    for line in fileinput.input(path+'/crab.status') :
+        if line.find("Dashboard:")>0 :
+            dash.append((line[line.find(":")+1:]).strip())
+    return '"'+', '.join(dash)+'"'
+
+def get_options(name) :
+    option = {}
+    timestamp = '_'.join(['%02d'% i for i in datetime.datetime.now().timetuple()[:6]])
+    print "Choose output site:"
+    for site in ["CASTOR","CAF","LONDON","OSETHACK", "FNAL"] : print '\t'+site
+    option["SITE"] = raw_input("\t> ")
+    option["SERVER"] = True if raw_input('Run Jobs via Server? [y/n]  ') in ['Y','y',1] else False
+    option["USER"] = getpass.getuser()
+    option["NODE"] = socket.gethostname()
+    option["PATH"] = '/'.join(['','tmp',option['USER'],name,timestamp,''])
+    option["RPATH"] = '/'.join(['',name,'automated',timestamp,''])
+    option["JOBID"] = job['rowid']
+    option['DATASET'] = job['dataset']
+    option["MULTI"] = len(job['dataset'].split(','))>1
+    option["DBS_URL"] = None
+
+    print 'You have specified:'
+    for key in ["SITE","SERVER"]:  print "%s : %s"%( key, str(option[key]))
+    return option
+
+db = conf.lockedDB()
+db.connect()
+job = get_jobrow( db )
+option = get_options(db.name)
+
+doit = raw_input('\nDo it? [y/n/f]')
+if doit in ['Y','y',1,'f','F'] :
+
+    setup_cmssw( job, option["PATH"] )
+    setup_crab( job, option)
+    if option["MULTI"] : setup_multi( job, option['PATH'] )
+    run_crab( job, option["PATH"], option['MULTI'], onlyTest = doit in ['f','F'])
+    option["DASH"] = get_dashboard(option["PATH"])
+    
+    db.execute('''update job set
+    state="Processing",
+    user="%(USER)s",
+    node="%(NODE)s",
+    path="%(PATH)s",
+    rpath="%(FULL_RPATH)s",
+    dash=%(DASH)s where rowid=%(JOBID)d''' % option )
+
+db.disconnect()
---
+++ scripts/bookkeeping/webpage_SCBooks.py
@@ -0,0 +1,187 @@
+import sqlite3,os,sys,getpass,time
+
+def print_HEAD(file,name) :
+    print>>file, '''
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html>
+    <head> 
+    <title> %s Datasets </title>'''%name +'''
+    <style type="text/css">
+    <!--
+    body {
+    text-align:left;
+    margin-left:30px;
+    }
+    .tagwrapper {
+    text-align:left;
+    margin:0 auto;
+    border:1px solid #ccc;
+    padding-left:30px;
+    padding-right:10px;
+    padding-bottom:5px;
+    }
+    .dsetwrapper {
+    text-align:left;
+    margin:0 auto;
+    border:1px solid #ccc;
+    padding-left:30px;
+    padding-right:10px;
+    padding-bottom:5px;
+    }
+    .jobwrapper {
+    text-align:left;
+    margin:0 auto;
+    border:1px solid #ccc;
+    padding-left:30px;
+    padding-right:10px;
+    padding-bottom:5px;
+    }
+    a {
+    color:blue;
+    cursor:pointer;
+    }
+    .Processing {
+    color:black;
+    background-color:yellow;
+    }
+    .Unclaimed {
+    color:black;
+    background-color:red;
+    }
+    .Complete {
+    color:black;
+    background-color:#00FF00;
+    }
+    -->
+    </style>
+    
+    <script type="text/javascript">
+    <!--
+    function switchMenu(obj) {
+    var el = document.getElementById(obj);
+    if ( el.style.display != "none" ) {
+         el.style.display = \'none\';
+    }
+    else {
+         el.style.display = '';
+    }
+    }
+    var jobSwitches = new Array();
+    var dsetSwitches = new Array();
+    var tagSwitches = new Array();
+
+    function switchAll(items) {
+      for( var i=0; i < items.length; i++) {
+        switchMenu(items[i])
+      }
+    }
+
+    function pop(text)
+    {
+      my_window = window.open("", "json", "status=1,width=500,height=300");
+      my_window.document.write(text);
+    }
+        
+
+    //-->
+    </script>
+
+    </head>
+    <body>
+    '''
+    return
+    
+
+def print_FOOT(file) :
+    print>>file,'''<script type="text/javascript"><!--
+    switchAll(jobSwitches)
+    switchAll(dsetSwitches)
+    switchAll(tagSwitches)
+    //-->
+    </script>
+    </body></html>'''
+
+def jsonDisplay(json) :
+    djson = eval(json)
+    runs = [eval(key) for key in djson]
+    minRun = min(runs)
+    maxRun = max(runs)
+    lumis = sum([1+pair[1]-pair[0] for pair in sum(djson.values(),[])])
+    jsonString = '\\{'+', '.join(['&quot;%d&quot;: %s'%pair for pair in sorted([(eval(key),val) for key,val in djson.iteritems()])]) + '\\}'
+    return 'Run Range <a onclick="return pop(\'%s\')">(%d,%d)</a> %d lumis'%(jsonString,minRun,maxRun,lumis)
+
+def print_JOB(file,job) :
+    label = 'job%d' % job['rowid']
+    print>>file,'\n'.join([
+        '''<script type="text/javascript"><!--
+        jobSwitches.push(\'%s\');//-->
+        </script>'''%label,
+        '<br><a onclick="switchMenu(\'%s\');" class="%s">' % (label,job['state']),
+        "%d:"%job['rowid'],
+        '</a>',
+        "&nbsp;%s"%(job['rpath'] if job['rpath'] else 'Unclaimed'),
+        '<div id="%s" class=jobwrapper>' % label,
+        ('<br>'+job['user']+'@'+job['node']+':'+job['path']) if job['user'] else '',
+        ('<br>Dashboard: ' + ', '.join(['<a href="%s">Job%d</a>' % (item, index) for index,item in enumerate(job['dash'].split(',')) ])) if job['dash'] else '',
+        '<br>'+jsonDisplay(job['jsonls']) if job['jsonls'] else ''
+        ])
+    print>>file,'</div>'
+    return
+
+def print_DSET(file,db,dset,recipe,gT) :
+    label= 'dset%d_%s' % (dset['rowid'],recipe)
+    jobs = db.execute('select rowid,* from job where dsetid=? and recipe=? and globalTag=? order by user',(dset['rowid'],recipe,gT)).fetchall()
+    if len(jobs)>0 :
+        print>>file,'\n'.join([
+            '''<script type="text/javascript"><!--
+            dsetSwitches.push(\'%s\');//-->
+            </script>'''%label,
+            '<br><a onclick="switchMenu(\'%s\');">' % label,
+            '<b>%s</b>' % dset['dataset'].replace(",","<br>"),
+            '</a>',
+            '<div id="%s" class=dsetwrapper>' % label,
+            ])
+        for job in jobs:
+            print_JOB(file,job)
+        print>>file,'</div>'
+    return
+
+def print_TAG(file,db,tag) :
+    label = 'recipe_%s' % tag['recipe']
+    print>>file,'\n'.join([
+        '''<script type="text/javascript"><!--
+        tagSwitches.push(\'%s\');//-->
+        </script>'''%label,
+        '<a onclick="switchMenu(\'%s\');">' % label,
+        '<b> TopRefTuple recipe %s'%(tag['recipe']),
+        '</a><a href="%s">*==></a>'%("http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/Betchart/TopRefTuple/scripts/recipe.sh?revision=%s&view=markup"%tag['recipe']),
+        '<div id="%s" class=tagwrapper>' % label,
+        ])
+    
+    print>>file,'<br><br>'
+    for gT in db.execute('select distinct globalTag from job where recipe=?',(tag['recipe'],)).fetchall() :
+        print>>file, '<br> %s'%gT['globalTag']
+        for dset in db.execute('select rowid,* from dset order by dataset, isData').fetchall() :
+            print_DSET(file,db,dset,tag['recipe'],gT['globalTag'])
+    print>>file,'</div>'
+    return
+
+def print_BODY(file,db) :
+    print>>file,'''
+    <div><div><a onclick="switchAll(tagSwitches);">Toggle Tags</a></div>
+    <div><a onclick="switchAll(dsetSwitches);">Toggle Datasets</a></div>
+    <div><a onclick="switchAll(jobSwitches);">Toggle Jobs</a></div></div>
+    '''
+    for tag in db.execute('select distinct recipe from job order by recipe desc').fetchall() :
+        print>>file,'<p>'
+        print_TAG(file,db,tag)
+        print>>file,'</p>'
+    return
+
+def write_webpage(db,path,name) :
+    file = open(path,"w")
+    print_HEAD(file,name)
+    print_BODY(file,db)
+    print_FOOT(file)
+    file.close()
+    return
