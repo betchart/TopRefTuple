a5f4c2e26346d77c9f25e7c9dffd9e7da061036e
bbetchar <>
2012-11-17 00:22:46 +0000

    tag

--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -1,10 +1,10 @@
 RELEASE=CMSSW_5_3_6_patch1
-cmsrel $RELEASE
+scram project CMSSW $RELEASE
 cd $RELEASE/src
-cmsenv
+eval `scram runtime -sh`
 addpkg RecoMET/METAnalyzers  V00-00-08
 addpkg RecoParticleFlow/PFProducer V15-02-06
-cvs co              -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
+cvs co -r V00-01-00 -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
 cvs co -r V00-00-13 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
 cd EGamma/EGammaAnalysisTools/data
 cat download.url | xargs wget
