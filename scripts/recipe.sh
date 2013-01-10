ecc9111a045225990dc59a730913717a3ceb406d
bbetchar <>
2013-01-10 07:30:59 +0000

    update recipe

--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -6,7 +6,7 @@ addpkg RecoMET/METFilters V00-00-13
 addpkg RecoMET/METAnalyzers  V00-00-08
 addpkg DPGAnalysis/SiStripTools V00-11-17
 cvs co -r V00-01-05 -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
-cvs co -r V00-00-31-EA -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
+cvs co -r V00-00-31-EA02 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
 cd EGamma/EGammaAnalysisTools/data
 cat download.url | xargs wget
 cd -
