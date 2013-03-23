d7fa748791f6803b74529b34af30054c4dfcb535
bbetchar <>
2013-03-23 03:08:38 +0000

    new tag

--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -5,7 +5,7 @@ eval `scram runtime -sh`
 addpkg RecoMET/METFilters V00-00-13
 addpkg RecoMET/METAnalyzers  V00-00-08
 addpkg DPGAnalysis/SiStripTools V00-11-17
-cvs co -r V00-03-01 -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
+cvs co -r V00-03-02 -d TopQuarkAnalysis/TopRefTuple UserCode/Betchart/TopRefTuple
 cvs co -r V00-00-31-EA02 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
 cd EGamma/EGammaAnalysisTools/data
 cat download.url | xargs wget
