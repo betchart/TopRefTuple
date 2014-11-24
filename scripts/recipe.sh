0f334eb1981182e5e82fc8ccefe987a65679daff
Burton Betchart <betchart@gmail.com>
2014-11-24 22:47:21 +0100

    testing tweak

--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -4,7 +4,6 @@ cd $RELEASE/src
 eval `scram runtime -sh`
 export CVSROOT=:ext:bbetchar@lxplus.cern.ch:/afs/cern.ch/user/c/cvscmssw/public/CMSSW
 
-#git cms-addpkg EgammaAnalysis/ElectronTools CMSSW_5_3_13
 git cms-addpkg EgammaAnalysis/ElectronTools
 cd EgammaAnalysis/ElectronTools/data/
 cat download.url | xargs wget
@@ -14,4 +13,6 @@ cvs co -r V00-00-13 RecoMET/METFilters
 cvs co -r V00-00-08 RecoMET/METAnalyzers
 cvs co -r V00-11-17 DPGAnalysis/SiStripTools
 
-git clone --branch V00-04-00 https://github.com/betchart/TopRefTuple.git TopQuarkAnalysis/TopRefTuple
+git clone --branch V00-04-02 https://github.com/betchart/TopRefTuple.git TopQuarkAnalysis/TopRefTuple
+
+### git cms-addpkg EgammaAnalysis/ElectronTools CMSSW_5_3_13
